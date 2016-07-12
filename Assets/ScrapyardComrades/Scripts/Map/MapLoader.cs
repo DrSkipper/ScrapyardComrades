eb28cfd75b5fb82a56da7e26af8e412700ccb221
Fletcher <fletcherc89@gmail.com>
2016-07-11 17:10:28 -0700

    Progress on level loading

---
+++ Assets/ScrapyardComrades/Scripts/Map/MapLoader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Newtonsoft.Json;
+
+public class MapLoader : MonoBehaviour
+{
+    public string MapName = "GameplayTest";
+    public TileRenderer PlatformsRenderer;
+
+    void Start()
+    {
+        TextAsset asset = Resources.Load<TextAsset>(PATH + this.MapName);
+        object data = JsonConvert.DeserializeObject(asset.text);
+    }
+
+    /**
+     * Private
+     */
+    private const string PATH = "Levels/";
+}
