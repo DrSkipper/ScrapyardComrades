0eb3086fb4c41fab7c425f1a9a70601f359a5a62
Fletcher <fletcherc89@gmail.com>
2016-08-16 00:22:02 -0700

    cleanup

--- Assets/ScrapyardComrades/Scripts/Map/WorldLoadingManager.cs
+++ Assets/ScrapyardComrades/Scripts/Map/WorldLoadingManager.cs
@@ -199,7 +199,6 @@ public class WorldLoadingManager : MonoBehaviour
 
         GameObject newMapLoaderObject = Instantiate(this.MapLoaderPrefab, position, Quaternion.identity) as GameObject;
         MapLoader newMapLoader = newMapLoaderObject.GetComponent<MapLoader>();
-        Debug.Log("New MapLoader position = " + newMapLoader.transform.position);
         this.MapLoaders.Add(newMapLoader);
         return newMapLoader;
     }
