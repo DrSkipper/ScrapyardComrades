31e871e8bd1ea5e80130a8ec9a86a66db8d2dd21
Fletcher <fletcherc89@gmail.com>
2016-11-14 23:33:16 -0800

    Simple pause screen

---
+++ Assets/ScrapyardComrades/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PausePanel;
+    public PauseController.PauseGroup PauseGroup;
+
+    void Awake()
+    {
+        GlobalEvents.Notifier.Listen(PauseEvent.NAME, this, onPause);
+        GlobalEvents.Notifier.Listen(ResumeEvent.NAME, this, onResume);
+    }
+
+    /**
+     * Private
+     */
+    private void onPause(LocalEventNotifier.Event e)
+    {
+        if ((e as PauseEvent).PauseGroup == this.PauseGroup)
+        {
+            this.PausePanel.SetActive(true);
+        }
+    }
+
+    public void onResume(LocalEventNotifier.Event e)
+    {
+        if ((e as ResumeEvent).PauseGroup == this.PauseGroup)
+        {
+            this.PausePanel.SetActive(false);
+        }
+    }
+}
