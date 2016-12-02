270880277894710fc7868e54917333117a4740b1
Fletcher <fletcherc89@gmail.com>
2016-12-01 16:10:33 -0800

    Parallaxing

--- Assets/ScrapyardComrades/Scripts/Visuals/ParallaxLayerController.cs
+++ Assets/ScrapyardComrades/Scripts/Visuals/ParallaxLayerController.cs
@@ -4,6 +4,18 @@ public class ParallaxLayerController : MonoBehaviour
 {
     public ParallaxQuadGroup CurrentLayerVisual;
     public ParallaxQuadGroup PreviousLayerVisual;
+    public Transform Tracker;
+    public float ParallaxRatio;
+
+    void Update()
+    {
+        // Move to position ParallaxRatio between 0,0 and tracker (camera) position
+        Vector2 trackerPos = this.Tracker.transform.position;
+        Vector2 trackerNormalized = trackerPos.normalized;
+        float magnitude = trackerPos.magnitude * this.ParallaxRatio;
+        IntegerVector final = trackerNormalized * magnitude;
+        this.transform.position = new Vector3(final.X, final.Y, this.transform.position.z);
+    }
 
     public void TransitionToNewLayer(SCParallaxLayer layer)
     {
