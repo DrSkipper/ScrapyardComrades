08f7028cd94ebac72698554a6d8daebb48468180
Fletcher <fletcherc89@gmail.com>
2017-03-04 16:38:00 -0800

    Map editor can apply static brush

---
+++ Assets/ScrapyardComrades/Scripts/Editing/MapEditorManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MapEditorManager : MonoBehaviour
+{
+    public struct Brush
+    {
+        public int Layer;
+        public int SpriteIndex; //TODO: Handle copy-paste brush (+ fill option?)
+    }
+
+    public MapEditorQuad Quad;
+    public MapEditorGrid Grid;
+    public MapEditorCursor Cursor;
+    public Texture2D[] ValidAtlases;
+    public Brush CurrentBrush;
+    public bool FlipVertical = true;
+
+    void Awake()
+    {
+        _atlases = MapLoader.CompileTextures(this.ValidAtlases);
+        _sprites = MapLoader.CompileSprites(_atlases);
+        this.Quad.Atlases = _atlases;
+        this.Quad.Sprites = _sprites;
+    }
+
+    void FixedUpdate()
+    {
+        if (MapEditorInput.Confirm)
+        {
+            //TODO: Handle different layers
+            this.Quad.PlatformsRenderer.SetSpriteIndexForTile(this.Cursor.GridPos.X, this.FlipVertical ? this.Grid.Height - this.Cursor.GridPos.Y - 1 : this.Cursor.GridPos.Y, this.CurrentBrush.SpriteIndex);
+        }
+    }
+
+    public Texture2D GetAtlasForBrush(Brush brush)
+    {
+        return _atlases[this.ValidAtlases[brush.Layer].name];
+    }
+
+    public Sprite GetSpriteForBrush(Brush brush)
+    {
+        return _sprites[this.ValidAtlases[brush.Layer].name][brush.SpriteIndex];
+    }
+
+    /**
+     * Private
+     */
+    private Dictionary<string, Texture2D> _atlases;
+    private Dictionary<string, Sprite[]> _sprites;
+}
--- Assets/ScrapyardComrades/Scripts/Map/MapEditorQuad.cs
+++ Assets/ScrapyardComrades/Scripts/Map/MapEditorQuad.cs
@@ -7,13 +7,14 @@ public class MapEditorQuad : MonoBehaviour
     public MapEditorGrid Grid;
     public TileRenderer PlatformsRenderer;
     public TileRenderer BGRenderer;
-    public Texture2D[] ValidAtlases;
     public string PlatformsLayer = "platforms";
     public string BGLayer = "background";
     public string ObjectsLayer = "objects";
     public bool LoadOnStart = false;
 
     public bool Cleared { get { return _cleared; } }
+    public Dictionary<string, Texture2D> Atlases;
+    public Dictionary<string, Sprite[]> Sprites;
 
     void Start()
     {
@@ -26,26 +27,20 @@ public class MapEditorQuad : MonoBehaviour
         this.ClearMap();
         _cleared = false;
 
-        if (_atlases == null || _atlases.Count == 0)
-        {
-            _atlases = MapLoader.CompileTextures(this.ValidAtlases);
-            _sprites = MapLoader.CompileSprites(_atlases);
-        }
-
         MapInfo mapInfo = MapLoader.GatherMapInfo(this.MapName);
         MapGridSpaceInfo[,] grid = mapInfo.GetLayerWithName(this.PlatformsLayer).GetGrid(mapInfo.tilesets);
         _width = mapInfo.width;
         _height = mapInfo.height;
         this.Grid.InitializeGridForSize(_width, _height);
-        this.PlatformsRenderer.Atlas = _atlases[this.PlatformsLayer];
-        this.PlatformsRenderer.Sprites = _sprites[this.PlatformsLayer];
+        this.PlatformsRenderer.Atlas = this.Atlases[this.PlatformsLayer];
+        this.PlatformsRenderer.Sprites = this.Sprites[this.PlatformsLayer];
         this.PlatformsRenderer.CreateMapWithGrid(grid);
         MapInfo.MapLayer bgLayer = mapInfo.GetLayerWithName(this.BGLayer);
         if (bgLayer != null)
         {
             MapGridSpaceInfo[,] bgGrid = bgLayer.GetGrid(mapInfo.tilesets);
-            this.BGRenderer.Atlas = _atlases[this.BGLayer];
-            this.BGRenderer.Sprites = _sprites[this.BGLayer];
+            this.BGRenderer.Atlas = this.Atlases[this.BGLayer];
+            this.BGRenderer.Sprites = this.Sprites[this.BGLayer];
             this.BGRenderer.CreateMapWithGrid(bgGrid);
         }
     }
@@ -69,8 +64,6 @@ public class MapEditorQuad : MonoBehaviour
      * Private
      */
     private const string PATH = "Levels/";
-    private Dictionary<string, Texture2D> _atlases;
-    private Dictionary<string, Sprite[]> _sprites;
     private bool _cleared = false;
     private int _width = 0;
     private int _height = 0;
