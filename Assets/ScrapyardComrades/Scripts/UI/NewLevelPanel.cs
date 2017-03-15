48a8c36c04b98f66dd8b70966ad090cdb849d788
Fletcher <fletcherc89@gmail.com>
2017-03-14 21:43:57 -0700

    Must press confirm on name entry box to begin typing

--- Assets/ScrapyardComrades/Scripts/UI/NewLevelPanel.cs
+++ Assets/ScrapyardComrades/Scripts/UI/NewLevelPanel.cs
@@ -13,11 +13,16 @@ public class NewLevelPanel : VoBehavior
 
     public delegate void OnLevelCreationComplete(string levelName, string platformsTileset, string backgroundTileset);
 
+    void Awake()
+    {
+        this.PlatformsPanel.Text.text = this.PlatformTilesets[0].name;
+        this.BackgroundPanel.Text.text = this.BackgroundTilesets[0].name;
+    }
+
     public void Show()
     {
         this.gameObject.SetActive(true);
-        selectPlatforms();
-        this.LevelNameField.ActivateInputField();
+        selectNameEntry();
     }
 
     void Update()
@@ -31,22 +36,58 @@ public class NewLevelPanel : VoBehavior
             this.CompletionCallback(levelName, platforms, background);
             this.gameObject.SetActive(false);
         }
-        else if (MapEditorInput.NavDown || MapEditorInput.NavUp)
+        else if (MapEditorInput.Confirm && _selection == NAME_ENTRY)
         {
-            if (_platformsSelected)
-                selectBackground();
+            if (_nameEntryEnabled)
+            {
+                _nameEntryEnabled = false;
+                this.LevelNameField.DeactivateInputField();
+            }
             else
-                selectPlatforms();
+            {
+                _nameEntryEnabled = true;
+                this.LevelNameField.ActivateInputField();
+            }
+        }
+        else if (!_nameEntryEnabled && (MapEditorInput.NavDown || MapEditorInput.NavUp))
+        {
+            
+            if (MapEditorInput.NavDown)
+            {
+                ++_selection;
+                if (_selection > BACKGROUND)
+                    _selection = NAME_ENTRY;
+            }
+            else
+            {
+                --_selection;
+                if (_selection < NAME_ENTRY)
+                    _selection = BACKGROUND;
+            }
+
+            switch (_selection)
+            {
+                default:
+                case NAME_ENTRY:
+                    selectNameEntry();
+                    break;
+                case PLATFORMS:
+                    selectPlatforms();
+                    break;
+                case BACKGROUND:
+                    selectBackground();
+                    break;
+            }
         }
         else if (MapEditorInput.NavLeft)
         {
-            if (_platformsSelected)
+            if (_selection == PLATFORMS)
             {
                 _selectedPlatforms = _selectedPlatforms <= 0 ? this.PlatformTilesets.Length - 1 : _selectedPlatforms - 1;
                 this.PlatformsPanel.Text.text = this.PlatformTilesets[_selectedPlatforms].name;
                 this.PlatformsPanel.CycleLeft();
             }
-            else
+            else if (_selection == BACKGROUND)
             {
                 _selectedBackground = _selectedBackground <= 0 ? this.BackgroundTilesets.Length - 1 : _selectedBackground - 1;
                 this.BackgroundPanel.Text.text = this.BackgroundTilesets[_selectedBackground].name;
@@ -55,13 +96,13 @@ public class NewLevelPanel : VoBehavior
         }
         else if (MapEditorInput.NavRight)
         {
-            if (_platformsSelected)
+            if (_selection == PLATFORMS)
             {
                 _selectedPlatforms = _selectedPlatforms >= this.PlatformTilesets.Length - 1 ? 0 : _selectedPlatforms + 1;
                 this.PlatformsPanel.Text.text = this.PlatformTilesets[_selectedPlatforms].name;
                 this.PlatformsPanel.CycleRight();
             }
-            else
+            else if (_selection == BACKGROUND)
             {
                 _selectedBackground = _selectedBackground >= this.BackgroundTilesets.Length - 1 ? 0 : _selectedBackground + 1;
                 this.BackgroundPanel.Text.text = this.BackgroundTilesets[_selectedBackground].name;
@@ -73,19 +114,29 @@ public class NewLevelPanel : VoBehavior
     /**
      * Private
      */
-    private bool _platformsSelected;
+    private int _selection;
     private int _selectedPlatforms;
     private int _selectedBackground;
+    private bool _nameEntryEnabled;
+    private const int NAME_ENTRY = 0;
+    private const int PLATFORMS = 1;
+    private const int BACKGROUND = 2;
+
+    private void selectNameEntry()
+    {
+        _selection = NAME_ENTRY;
+        this.Selector.anchoredPosition = new Vector2(this.Selector.anchoredPosition.x, ((RectTransform)this.LevelNameField.transform).anchoredPosition.y);
+    }
 
     private void selectPlatforms()
     {
-        _platformsSelected = true;
+        _selection = PLATFORMS;
         this.Selector.anchoredPosition = new Vector2(this.Selector.anchoredPosition.x, ((RectTransform)this.PlatformsPanel.transform).anchoredPosition.y);
     }
 
     private void selectBackground()
     {
-        _platformsSelected = false;
+        _selection = BACKGROUND;
         this.Selector.anchoredPosition = new Vector2(this.Selector.anchoredPosition.x, ((RectTransform)this.BackgroundPanel.transform).anchoredPosition.y);
     }
 }
