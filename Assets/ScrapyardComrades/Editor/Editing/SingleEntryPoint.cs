2a7f4280552d77d9dc209aab0139e763319a4b32
Fletcher <fletcherc89@gmail.com>
2017-04-20 18:22:40 -0700

    Automatically index sprites before running game!

---
+++ Assets/ScrapyardComrades/Editor/Editing/SingleEntryPoint.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+
+//Credit: http://answers.unity3d.com/questions/447701/event-for-unity-editor-pause-and-playstop-events.html
+[InitializeOnLoad]
+public class SingleEntryPoint
+{
+    static SingleEntryPoint()
+    {
+        //Debug.Log("SingleEntryPoint. Up and running");
+        EditorPlayMode.PlayModeChanged += OnPlayModeChanged;
+    }
+
+    private static void OnPlayModeChanged(PlayModeState currentMode, PlayModeState changedMode)
+    {
+        //Debug.Log("play mode changed");
+        if (changedMode == PlayModeState.AboutToPlay)
+            PackedSpriteGroupEditor.AggregateAllAtlases();
+    }
+}
