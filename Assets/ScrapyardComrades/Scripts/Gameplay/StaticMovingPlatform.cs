31f590f9d4f8a2a111775bd3cf25c7edc7d50ad3
Fletcher <fletcherc89@gmail.com>
2017-05-16 16:42:10 -0700

    Added conveyor belts

---
+++ Assets/ScrapyardComrades/Scripts/Gameplay/StaticMovingPlatform.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class StaticMovingPlatform : VoBehavior, IMovingPlatform
+{
+    public Vector2 StaticVelociy;
+    public Vector2 Velocity { get { return this.StaticVelociy; } }
+
+    void OnSpawn()
+    {
+        this.integerCollider.AddToCollisionPool();
+    }
+    
+    void OnReturnToPool()
+    {
+        this.integerCollider.RemoveFromCollisionPool();
+    }
+}
