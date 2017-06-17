052d1804446dda411163911efdd0b600f91b8710
Fletcher <fletcherc89@gmail.com>
2017-06-16 18:41:00 -0700

    Tracking version number (at Alpha 0.1.0 now), and displaying on main menu

---
+++ Assets/ScrapyardComrades/Scripts/Data/VersionNumber.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public static class VersionNumber
+{
+    public static string VERSION_TITLE = "Alpha";
+    public static int MAJOR_VERSION = 0;
+    public static int MINOR_VERSION = 1;
+    public static int PATCH_NUMBER = 0;
+
+    public static string FullVersionString
+    {
+        get
+        {
+            return VERSION_TITLE + StringExtensions.SPACE + MAJOR_VERSION + StringExtensions.PERIOD + MINOR_VERSION + StringExtensions.PERIOD + PATCH_NUMBER;
+        }
+    }
+}
