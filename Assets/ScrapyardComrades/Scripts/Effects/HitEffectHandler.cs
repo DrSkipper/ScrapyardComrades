36eb9e7c628dcb8b79037653e542e8170f7d485b
Fletcher <fletcherc89@gmail.com>
2017-07-28 13:40:21 -0700

    Replaced main hit effect for now

--- Assets/ScrapyardComrades/Scripts/Effects/HitEffectHandler.cs
+++ Assets/ScrapyardComrades/Scripts/Effects/HitEffectHandler.cs
@@ -2,34 +2,38 @@
 
 public class HitEffectHandler : MonoBehaviour, IPausable
 {
-    public Animator Animator;
+    public SCSpriteAnimator Animator;
 
     public void InitializeWithFreezeFrames(int freezeFrames)
     {
+        _animStarted = false;
+        this.Animator.PlayAnimation(this.Animator.DefaultAnimation);
+        this.Animator.Stop();
         _freezeTimer = new Timer(freezeFrames, false, true);
     }
 
     void FixedUpdate()
     {
-        if (_freezeTimer != null)
+        if (!_animStarted)
         {
             _freezeTimer.update();
             if (_freezeTimer.Completed)
-                this.Animator.SetTrigger(BEGIN_TRIGGER);
+            {
+                _animStarted = true;
+                this.Animator.PlayAnimation(this.Animator.DefaultAnimation);
+            }
+        }
+        else
+        {
+            if (!this.Animator.IsPlaying)
+            {
+                _animStarted = false;
+                ObjectPools.Release(this.gameObject);
+            }
         }
     }
 
-    public void EffectCompleted()
-    {
-        ObjectPools.Release(this.gameObject);
-    }
-
-    void OnReturnToPool()
-    {
-        this.Animator.ResetTrigger(BEGIN_TRIGGER);
-        this.Animator.Play(STARTING_STATE);
-    }
-
+    private bool _animStarted;
     private Timer _freezeTimer;
     private const string STARTING_STATE = "Empty";
     private const string BEGIN_TRIGGER = "Begin";
