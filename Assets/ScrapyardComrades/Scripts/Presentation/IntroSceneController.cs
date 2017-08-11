479df0bd722c5a43bdf605264861f9038196877b
Fletcher <fletcherc89@gmail.com>
2017-08-11 13:08:30 -0700

    started work on intro sequence

---
+++ Assets/ScrapyardComrades/Scripts/Presentation/IntroSceneController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroSceneController : MonoBehaviour
+{
+    public int FadeDuration = 40;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start()
+    {
+        _fadeInEvent = new MainMenuBGSwapEvent(this.FadeDuration);
+        _fadeOutEvent = new MainMenuBGFadeEvent(this.FadeDuration);
+        GlobalEvents.Notifier.SendEvent(_fadeInEvent);
+    }
+
+    /**
+     * Private
+     */
+    private MainMenuBGSwapEvent _fadeInEvent;
+    private MainMenuBGFadeEvent _fadeOutEvent;
+}
