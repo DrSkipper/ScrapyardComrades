466f393194b1b3c65676037bb101df924b8f33c9
Fletcher <fletcherc89@gmail.com>
2017-08-28 19:32:51 -0700

    Simple particle system, ambulance smoke

---
+++ Assets/ScrapyardComrades/Scripts/Presentation/SimpleParticle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class SimpleParticle : VoBehavior
+{
+    public SCSpriteAnimator Animator;
+    public delegate void OnCompleteDelegate(SimpleParticle particle);
+
+    public void Emit(SCSpriteAnimation animation, OnCompleteDelegate onComplete)
+    {
+        this.Animator.PlayAnimation(animation);
+        _onComplete = onComplete;
+    }
+
+    void FixedUpdate()
+    {
+        if (!this.Animator.IsPlaying)
+            _onComplete(this);
+    }
+
+    /**
+     * Private
+     */
+    private OnCompleteDelegate _onComplete;
+}
---
+++ Assets/ScrapyardComrades/Scripts/Presentation/SimpleParticleEmitter.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SimpleParticleEmitter : MonoBehaviour, IPausable
+{
+    public IntegerCollider EmmissionRange;
+    public SimpleParticle[] PooledParticles;
+    public SCSpriteAnimation[] ParticleAnimations;
+    public IntegerVector SpawnIntervalRange;
+
+    void Awake()
+    {
+        _used = new List<SimpleParticle>();
+        _unused = new List<SimpleParticle>();
+    }
+
+    void OnSpawn()
+    {
+        _t = Random.Range(this.SpawnIntervalRange.X, this.SpawnIntervalRange.Y + 1);
+        _used.Clear();
+        _unused.Clear();
+
+        for (int i = 0; i < this.PooledParticles.Length; ++i)
+        {
+            this.PooledParticles[i].gameObject.SetActive(false);
+            _unused.Add(this.PooledParticles[i]);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (_t <= 0)
+        {
+            _unused.Shuffle();
+            SimpleParticle particle = _unused.Pop();
+
+            if (particle != null)
+            {
+                _t = Random.Range(this.SpawnIntervalRange.X, this.SpawnIntervalRange.Y + 1);
+
+                IntegerVector pos = new IntegerVector(Random.Range(this.EmmissionRange.Bounds.Min.X, this.EmmissionRange.Bounds.Max.X), Random.Range(this.EmmissionRange.Bounds.Min.Y, this.EmmissionRange.Bounds.Max.Y));
+                emitParticle(particle, pos);
+            }
+        }
+        else
+        {
+            _t -= 1;
+        }
+    }
+
+    /**
+     * Private
+     */
+    private List<SimpleParticle> _used;
+    private List<SimpleParticle> _unused;
+    private int _t;
+
+    private void emitParticle(SimpleParticle particle, IntegerVector pos)
+    {
+        particle.gameObject.SetActive(true);
+        particle.transform.SetPosition2D(pos);
+        particle.Emit(this.ParticleAnimations[Random.Range(0, this.ParticleAnimations.Length)], onParticleComplete);
+        _unused.Remove(particle);
+        if (!_used.Contains(particle))
+            _used.Add(particle);
+    }
+
+    private void onParticleComplete(SimpleParticle particle)
+    {
+        _used.Remove(particle);
+        if (!_unused.Contains(particle))
+            _unused.Add(particle);
+
+        particle.gameObject.SetActive(false);
+    }
+}
