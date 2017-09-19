bd495664f22ec2294048097ca7e469d151ecb179
Fletcher <fletcherc89@gmail.com>
2017-09-19 16:36:02 -0700

    Can configure fan to attach at any cardinal direction

---
+++ Assets/ScrapyardComrades/Scripts/Editing/ObjectConfigurers/FanConfigurer.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class FanConfigurer : ObjectConfigurer
+{
+    public const string NAME = "Fan";
+    private const string ATTACH_DIR_TYPE = "attach";
+    private const string ATTACH_DOWN = "d";
+    private const string ATTACH_UP = "u";
+    private const string ATTACH_LEFT = "l";
+    private const string ATTACH_RIGHT = "r";
+
+    public WindFan FanScript;
+
+    public override ObjectParamType[] ParameterTypes
+    {
+        get
+        {
+            return new ObjectParamType[] {
+                new ObjectParamType(ATTACH_DIR_TYPE, new string[] {
+                    ATTACH_DOWN,
+                    ATTACH_UP,
+                    ATTACH_LEFT,
+                    ATTACH_RIGHT
+                })
+            };
+        }
+    }
+
+    protected override void ConfigureParameter(string parameterName, string option)
+    {
+        switch (parameterName)
+        {
+            default:
+                LogInvalidParameter(NAME, parameterName, option);
+                break;
+            case ATTACH_DIR_TYPE:
+                configureAttachType(option);
+                break;
+        }
+    }
+
+    /**
+     * Private
+     */
+    private void configureAttachType(string option)
+    {
+        switch (option)
+        {
+            default:
+                LogInvalidParameter(NAME, ATTACH_DIR_TYPE, option);
+                break;
+            case ATTACH_DOWN:
+                this.FanScript.AttachedAt = TurretController.AttachDir.Down;
+                break;
+            case ATTACH_UP:
+                this.FanScript.AttachedAt = TurretController.AttachDir.Up;
+                break;
+            case ATTACH_LEFT:
+                this.FanScript.AttachedAt = TurretController.AttachDir.Left;
+                break;
+            case ATTACH_RIGHT:
+                this.FanScript.AttachedAt = TurretController.AttachDir.Right;
+                break;
+        }
+    }
+}
---
+++ Assets/ScrapyardComrades/Scripts/Gameplay/WindFan.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+public class WindFan : MonoBehaviour
+{
+    public Transform SpriteTransform;
+    public WindRegion WindRegionScript;
+    public IntegerRectCollider WindRegionCollider;
+    public IntegerRectCollider ParticleSpawnRect;
+    public MoveAndFadeParticleEmitter Particles;
+
+    public TurretController.AttachDir AttachedAt = TurretController.AttachDir.Down;
+
+    void Awake()
+    {
+        _defaultRegionOffset = this.WindRegionCollider.Offset;
+        _defaultRegionSize = this.WindRegionCollider.Size;
+        _defaultParticleSpawnOffset = this.ParticleSpawnRect.Offset;
+        _defaultParticleSpawnSize = this.ParticleSpawnRect.Size;
+        _defaultParticleVelocity = this.Particles.PooledParticles[0].GetComponent<MoveAndFadeParticle>().VelocityDir;
+        _defaultTargetVelocity = this.WindRegionScript.TargetVelocity;
+    }
+
+    void OnSpawn()
+    {
+        IntegerVector particleVelocityDir;
+        switch (this.AttachedAt)
+        {
+            default:
+            case TurretController.AttachDir.Down:
+                this.SpriteTransform.rotation = Quaternion.Euler(0, 0, 0);
+                this.WindRegionCollider.Offset = _defaultRegionOffset;
+                this.WindRegionCollider.Size = _defaultRegionSize;
+                this.ParticleSpawnRect.Offset = _defaultParticleSpawnOffset;
+                this.ParticleSpawnRect.Size = _defaultParticleSpawnSize;
+                particleVelocityDir = _defaultParticleVelocity;
+                this.WindRegionScript.TargetVelocity = _defaultTargetVelocity;
+                break;
+            case TurretController.AttachDir.Up:
+                this.SpriteTransform.rotation = Quaternion.Euler(0, 0, 180);
+                this.WindRegionCollider.Offset = new IntegerVector(-_defaultRegionOffset.X, -_defaultRegionOffset.Y);
+                this.WindRegionCollider.Size = _defaultRegionSize;
+                this.ParticleSpawnRect.Offset = new IntegerVector(-_defaultParticleSpawnOffset.X, -_defaultParticleSpawnOffset.Y);
+                this.ParticleSpawnRect.Size = _defaultParticleSpawnSize;
+                particleVelocityDir = new IntegerVector(-_defaultParticleVelocity.X, -_defaultParticleVelocity.Y);
+                this.WindRegionScript.TargetVelocity = new Vector2(-_defaultTargetVelocity.x, -_defaultTargetVelocity.y);
+                break;
+            case TurretController.AttachDir.Left:
+                this.SpriteTransform.rotation = Quaternion.Euler(0, 0, -90);
+                this.WindRegionCollider.Offset = new IntegerVector(_defaultRegionOffset.Y, _defaultRegionOffset.X);
+                this.WindRegionCollider.Size = new IntegerVector(_defaultRegionSize.Y, _defaultRegionSize.X);
+                this.ParticleSpawnRect.Offset = new IntegerVector(_defaultParticleSpawnOffset.Y, _defaultParticleSpawnOffset.X);
+                this.ParticleSpawnRect.Size = new IntegerVector(_defaultParticleSpawnSize.Y, _defaultParticleSpawnSize.X);
+                particleVelocityDir = new IntegerVector(_defaultParticleVelocity.Y, _defaultParticleVelocity.X);
+                this.WindRegionScript.TargetVelocity = new Vector2(_defaultTargetVelocity.y, _defaultTargetVelocity.x);
+                break;
+            case TurretController.AttachDir.Right:
+                this.SpriteTransform.rotation = Quaternion.Euler(0, 0, 90);
+                this.WindRegionCollider.Offset = new IntegerVector(-_defaultRegionOffset.Y, -_defaultRegionOffset.X);
+                this.WindRegionCollider.Size = new IntegerVector(_defaultRegionSize.Y, _defaultRegionSize.X);
+                this.ParticleSpawnRect.Offset = new IntegerVector(-_defaultParticleSpawnOffset.Y, -_defaultParticleSpawnOffset.X);
+                this.ParticleSpawnRect.Size = new IntegerVector(_defaultParticleSpawnSize.Y, _defaultParticleSpawnSize.X);
+                particleVelocityDir = new IntegerVector(-_defaultParticleVelocity.Y, -_defaultParticleVelocity.X);
+                this.WindRegionScript.TargetVelocity = new Vector2(-_defaultTargetVelocity.y, -_defaultTargetVelocity.x);
+                break;
+        }
+
+        for (int i = 0; i < this.Particles.PooledParticles.Length; ++i)
+        {
+            this.Particles.PooledParticles[i].GetComponent<MoveAndFadeParticle>().VelocityDir = particleVelocityDir;
+        }
+    }
+
+    /**
+     * Private
+     */
+    private IntegerVector _defaultRegionOffset;
+    private IntegerVector _defaultRegionSize;
+    private IntegerVector _defaultParticleSpawnOffset;
+    private IntegerVector _defaultParticleSpawnSize;
+    private IntegerVector _defaultParticleVelocity;
+    private Vector2 _defaultTargetVelocity;
+}
