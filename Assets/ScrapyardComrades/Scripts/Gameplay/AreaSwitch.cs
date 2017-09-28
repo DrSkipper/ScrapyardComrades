b2f0968d3bb11577f904cd82bc81d64f5866e48f
Fletcher <fletcherc89@gmail.com>
2017-09-27 19:18:41 -0700

    Area detection switch code

---
+++ Assets/ScrapyardComrades/Scripts/Gameplay/AreaSwitch.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class AreaSwitch : MonoBehaviour
+{
+    public IntegerCollider DetectionCollider;
+    public Switch SwitchScript;
+    public LayerMask DetectionLayers;
+    public bool OneWay = false;
+
+    void FixedUpdate()
+    {
+        GameObject collided = this.DetectionCollider.CollideFirst(0, 0, this.DetectionLayers, null);
+
+        if (!_beingPressed)
+        {
+            if (collided != null)
+            {
+                _beingPressed = true;
+                if (canSwitch())
+                    this.SwitchScript.ToggleSwitch();
+            }
+        }
+        else
+        {
+            if (collided == null)
+            {
+                _beingPressed = false;
+                if (canSwitch())
+                    this.SwitchScript.ToggleSwitch();
+            }
+        }
+    }
+
+    /**
+     * Private
+     */
+    private bool _beingPressed;
+
+    private bool canSwitch()
+    {
+        return !this.OneWay || this.SwitchScript.CurrentState == this.SwitchScript.DefaultState;
+    }
+}
