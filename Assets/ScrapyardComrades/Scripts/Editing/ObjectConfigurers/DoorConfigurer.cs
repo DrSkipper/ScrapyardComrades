fe6cfa22ed63a4d23767eb8d38d24a225f12311e
Fletcher <fletcherc89@gmail.com>
2017-11-03 17:58:38 -0700

    Adding doors that are based on switches, though not fully functional yet

--- Assets/ScrapyardComrades/Scripts/Editing/ObjectConfigurers/DoorConfigurer.cs
+++ Assets/ScrapyardComrades/Scripts/Editing/ObjectConfigurers/DoorConfigurer.cs
@@ -14,7 +14,8 @@ public class DoorConfigurer : ObjectConfigurer
     public SCSpriteAnimation PurpCloseAnimation;
     public SCSpriteAnimation BlueOpenAnimation;
     public SCSpriteAnimation BlueCloseAnimation;
-    public Door DoorScript;
+    public DoorSwitchable DoorScript;
+    public SwitchListener SwitchListenerScript;
 
     public override ObjectParamType[] ParameterTypes
     {
@@ -25,6 +26,26 @@ public class DoorConfigurer : ObjectConfigurer
                     RED_DOOR,
                     PURP_DOOR,
                     BLUE_DOOR
+                }),
+                new ObjectParamType(SwitchConfigurer.INVERSE_SWITCH, new string[] {
+                    SwitchConfigurer.FALSE,
+                    SwitchConfigurer.TRUE
+                }),
+                new ObjectParamType(SwitchConfigurer.SWITCH_NAME, new string[] {
+                    SwitchConfigurer.SWITCH_A,
+                    SwitchConfigurer.SWITCH_B,
+                    SwitchConfigurer.SWITCH_C,
+                    SwitchConfigurer.SWITCH_D,
+                    SwitchConfigurer.SWITCH_E,
+                    SwitchConfigurer.SWITCH_F
+                }),
+                new ObjectParamType(SwitchConfigurer.DEFAULT_STATE, new string[] {
+                    Switch.OFF,
+                    Switch.ON
+                }),
+                new ObjectParamType(FanConfigurer.IGNORE_EVENTS, new string[] {
+                    SwitchConfigurer.FALSE,
+                    SwitchConfigurer.TRUE
                 })
             };
         }
@@ -40,6 +61,18 @@ public class DoorConfigurer : ObjectConfigurer
             case DOOR_TYPE:
                 configureDoorType(option);
                 break;
+            case SwitchConfigurer.SWITCH_NAME:
+                configureSwitchName(option);
+                break;
+            case SwitchConfigurer.INVERSE_SWITCH:
+                configureInverseSwitch(option);
+                break;
+            case SwitchConfigurer.DEFAULT_STATE:
+                configureDefaultState(option);
+                break;
+            case FanConfigurer.IGNORE_EVENTS:
+                configureIgnoreEvents(option);
+                break;
         }
     }
 
@@ -52,22 +85,78 @@ public class DoorConfigurer : ObjectConfigurer
         {
             default:
                 LogInvalidParameter(NAME, DOOR_TYPE, option);
-                this.DoorScript.LockType = SCPickup.KeyType.None;
+                //this.DoorScript.LockType = SCPickup.KeyType.None;
                 break;
             case RED_DOOR:
                 this.DoorScript.OpenAnimation = this.RedOpenAnimation;
                 this.DoorScript.CloseAnimation = this.RedCloseAnimation;
-                this.DoorScript.LockType = SCPickup.KeyType.Red;
+                //this.DoorScript.LockType = SCPickup.KeyType.Red;
                 break;
             case PURP_DOOR:
                 this.DoorScript.OpenAnimation = this.PurpOpenAnimation;
                 this.DoorScript.CloseAnimation = this.PurpCloseAnimation;
-                this.DoorScript.LockType = SCPickup.KeyType.Purple;
+                //this.DoorScript.LockType = SCPickup.KeyType.Purple;
                 break;
             case BLUE_DOOR:
                 this.DoorScript.OpenAnimation = this.BlueOpenAnimation;
                 this.DoorScript.CloseAnimation = this.BlueCloseAnimation;
-                this.DoorScript.LockType = SCPickup.KeyType.Blue;
+                //this.DoorScript.LockType = SCPickup.KeyType.Blue;
+                break;
+        }
+    }
+
+    private void configureSwitchName(string option)
+    {
+        this.SwitchListenerScript.SwitchName = option;
+    }
+
+    private void configureInverseSwitch(string option)
+    {
+        switch (option)
+        {
+            default:
+                LogInvalidParameter(NAME, SwitchConfigurer.INVERSE_SWITCH, option);
+                this.SwitchListenerScript.InversedSwitch = false;
+                break;
+            case SwitchConfigurer.TRUE:
+                this.SwitchListenerScript.InversedSwitch = true;
+                break;
+            case SwitchConfigurer.FALSE:
+                this.SwitchListenerScript.InversedSwitch = false;
+                break;
+        }
+    }
+
+    private void configureDefaultState(string option)
+    {
+        switch (option)
+        {
+            default:
+                LogInvalidParameter(NAME, SwitchConfigurer.DEFAULT_STATE, option);
+                this.SwitchListenerScript.DefaultState = Switch.SwitchState.OFF;
+                break;
+            case Switch.ON:
+                this.SwitchListenerScript.DefaultState = Switch.SwitchState.ON;
+                break;
+            case Switch.OFF:
+                this.SwitchListenerScript.DefaultState = Switch.SwitchState.OFF;
+                break;
+        }
+    }
+
+    private void configureIgnoreEvents(string option)
+    {
+        switch (option)
+        {
+            default:
+                LogInvalidParameter(NAME, FanConfigurer.IGNORE_EVENTS, option);
+                this.SwitchListenerScript.IgnoreEvents = false;
+                break;
+            case SwitchConfigurer.TRUE:
+                this.SwitchListenerScript.IgnoreEvents = true;
+                break;
+            case SwitchConfigurer.FALSE:
+                this.SwitchListenerScript.IgnoreEvents = false;
                 break;
         }
     }
---
+++ Assets/ScrapyardComrades/Scripts/Gameplay/DoorSwitchable.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+
+public class DoorSwitchable : VoBehavior
+{
+    public LayerMask BlockDoorMask;
+    public SCSpriteAnimator Animator;
+    public SCSpriteAnimation OpenAnimation;
+    public SCSpriteAnimation CloseAnimation;
+
+    private void Awake()
+    {
+        this.GetComponent<SwitchListener>().StateChangeCallback += onStateChange;
+        _stateTransitionCooldown = new Timer(STATE_TRANSITION_COOLDOWN, false, false);
+        _targetSwitchState = Switch.SwitchState.ON;
+        _currentSwitchState = Switch.SwitchState.ON;
+    }
+
+    void OnSpawn()
+    {
+        this.integerCollider.AddToCollisionPool();
+        _stateTransitionCooldown.complete();
+    }
+
+    void OnReturnToPool()
+    {
+        close();
+        _targetSwitchState = Switch.SwitchState.ON;
+        _currentSwitchState = Switch.SwitchState.ON;
+        this.integerCollider.RemoveFromCollisionPool();
+    }
+
+    void FixedUpdate()
+    {
+        if (_stateTransitionCooldown.Completed && _currentSwitchState != _targetSwitchState)
+        {
+            attemptAlignToSwitchState();
+        }
+    }
+
+    /**
+     * Private
+     */
+    private bool _open;
+    private Timer _stateTransitionCooldown;
+    private const int STATE_TRANSITION_COOLDOWN = 20;
+    private Switch.SwitchState _targetSwitchState;
+    private Switch.SwitchState _currentSwitchState;
+
+    private void onStateChange(Switch.SwitchState state)
+    {
+        _targetSwitchState = state;
+    }
+
+    private void attemptAlignToSwitchState()
+    {
+        switch (_targetSwitchState)
+        {
+            default:
+            case Switch.SwitchState.OFF:
+                open();
+                _currentSwitchState = _targetSwitchState;
+                break;
+            case Switch.SwitchState.ON:
+                if (tryToClose())
+                    _currentSwitchState = _targetSwitchState;
+                break;
+        }
+    }
+
+    private void open()
+    {
+        _open = true;
+        _stateTransitionCooldown.reset();
+        _stateTransitionCooldown.start();
+        this.integerCollider.enabled = false;
+        this.Animator.PlayAnimation(this.OpenAnimation);
+    }
+
+    private bool tryToClose()
+    {
+        GameObject collided = this.integerCollider.CollideFirst(0, 0, this.BlockDoorMask);
+
+        if (collided == null)
+        {
+            close();
+            return true;
+        }
+        return false;
+    }
+
+    private void close()
+    {
+        _open = false;
+        _stateTransitionCooldown.reset();
+        _stateTransitionCooldown.start();
+        this.integerCollider.enabled = true;
+        this.Animator.PlayAnimation(this.CloseAnimation);
+    }
+}
