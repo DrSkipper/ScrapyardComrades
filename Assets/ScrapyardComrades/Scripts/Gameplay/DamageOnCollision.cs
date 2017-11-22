cd8ec1879e1225a11c53bd219dbc8ff4f881c36f
Fletcher <fletcherc89@gmail.com>
2017-11-21 17:46:41 -0800

    Paramedic syringe impact effects

--- Assets/ScrapyardComrades/Scripts/Gameplay/DamageOnCollision.cs
+++ Assets/ScrapyardComrades/Scripts/Gameplay/DamageOnCollision.cs
@@ -6,10 +6,8 @@ public class DamageOnCollision : VoBehavior
     public SCAttack.HitData HitData;
     public float MinVelocityToDamage = 3.0f;
     public bool AlwaysDestroyOnCollide = false;
-    public PooledObject DestructionPrefab; //TODO
-    public SCSpriteAnimation DestructionAnimation;
     public PooledObject HitEffectPrefab;
-    public SCSpriteAnimation HitEffectAnimation;
+    public SCSpriteAnimation DestructionAnimation;
 
     void Awake()
     {
@@ -32,15 +30,18 @@ public class DamageOnCollision : VoBehavior
     private void onCollide(LocalEventNotifier.Event e)
     {
         Vector2 totalV = this.Actor.TotalVelocity;
+        bool hitTarget = false;
 
         if (totalV.magnitude >= this.MinVelocityToDamage)
         {
             CollisionEvent ce = e as CollisionEvent;
             for (int i = 0; i < ce.Hits.Count; ++i)
             {
-                LayerMask collisionMask = 1 << ce.Hits[i].layer;
+                GameObject hit = ce.Hits[i];
+                LayerMask collisionMask = 1 << hit.layer;
                 if ((collisionMask & this.DamagableLayers) == collisionMask)
                 {
+                    hitTarget = true;
                     ce.Hits[i].GetComponent<Damagable>().Damage(
                         this.HitData,
                         _prevPos,
@@ -48,11 +49,26 @@ public class DamageOnCollision : VoBehavior
                         this.Actor.TotalVelocity.x > 0 ?
                         SCCharacterController.Facing.Right :
                         SCCharacterController.Facing.Left);
+
+                    if (this.HitEffectPrefab != null)
+                    {
+                        PooledObject hitEffect = this.HitEffectPrefab.Retain();
+                        hitEffect.transform.SetPosition2D((hit.transform.position + this.transform.position) / 2);
+                        hitEffect.GetComponent<HitEffectHandler>().InitializeWithFreezeFrames(Damagable.FREEZE_FRAMES, this.HitData.HitAnimation, Mathf.RoundToInt(Mathf.Sign(hit.transform.position.x - this.transform.position.x)));
+                    }
                 }
             }
         }
 
         if (this.AlwaysDestroyOnCollide)
+        {
+            if (this.DestructionAnimation != null)
+            {
+                PooledObject effect = this.HitEffectPrefab.Retain();
+                effect.transform.SetPosition2D(this.transform.position);
+                effect.GetComponent<HitEffectHandler>().InitializeWithFreezeFrames(hitTarget ? Damagable.FREEZE_FRAMES : 0, this.DestructionAnimation, 1);
+            }
             ObjectPools.Release(this.gameObject);
+        }
     }
 }
