0c4d880a75ca559e2cf3b7b3e5c619917b3fa0f3
Fletcher <fletcherc89@gmail.com>
2017-12-07 16:53:14 -0800

    Script for trigger boxes

---
+++ Assets/ScrapyardComrades/Scripts/Gameplay/TriggerBox.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class TriggerBox : VoBehavior
+{
+    public string EventToSend = "TRIGGER";
+    public LayerMask TriggerMask;
+
+    void OnSpawn()
+    {
+        if (this.spriteRenderer != null)
+            this.spriteRenderer.enabled = false;
+    }
+
+    void OnReturnToPool()
+    {
+        if (this.spriteRenderer != null)
+            this.spriteRenderer.enabled = true;
+    }
+
+    void FixedUpdate()
+    {
+        if (this.integerCollider.CollideFirst(0, 0, this.TriggerMask))
+        {
+            LocalEventNotifier.Event e = new LocalEventNotifier.Event();
+            e.Name = this.EventToSend;
+            GlobalEvents.Notifier.SendEvent(e);
+            this.GetComponent<WorldEntity>().TriggerConsumption(true);
+        }
+    }
+}
