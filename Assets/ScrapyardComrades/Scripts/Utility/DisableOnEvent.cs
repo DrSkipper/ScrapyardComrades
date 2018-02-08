c97804731fff40a52c3e16a100790ddd491f5aad
Fletcher <fletcherc89@gmail.com>
2018-02-08 15:33:47 -0800

    Fixed triggering logo splash dismissal multiple times if buttons keep getting pressed

---
+++ Assets/ScrapyardComrades/Scripts/Utility/DisableOnEvent.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class DisableOnEvent : MonoBehaviour
+{
+    public bool ListenOnAwake = true;
+    public string EventName = "START_PRESSED";
+    public MonoBehaviour[] Behaviors;
+    public bool Enable = false;
+
+    void Awake()
+    {
+        if (this.ListenOnAwake)
+            GlobalEvents.Notifier.Listen(this.EventName, this, onEvent);
+    }
+
+    void OnSpawn()
+    {
+        if (!this.ListenOnAwake)
+            GlobalEvents.Notifier.Listen(this.EventName, this, onEvent);
+    }
+
+    void OnReturnToPool()
+    {
+        if (!this.ListenOnAwake)
+            GlobalEvents.Notifier.RemoveListenersForOwnerAndEventName(this, this.EventName);
+    }
+
+    private void onEvent(LocalEventNotifier.Event e)
+    {
+        for (int i = 0; i < this.Behaviors.Length; ++i)
+        {
+            this.Behaviors[i].enabled = this.Enable;
+        }
+    }
+}
