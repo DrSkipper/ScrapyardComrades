0749336c76e70c3ec3f94671973a4cf8be24af57
Fletcher <fletcherc89@gmail.com>
2018-02-23 16:58:48 -0800

    Working on patrolling platforms

---
+++ Assets/ScrapyardComrades/Scripts/Gameplay/PatrollingPlatform.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PatrollingPlatform : VoBehavior, IMovingPlatform
+{
+    public int Speed = 2;
+    public Transform Start;
+    public Transform Destination;
+    public Vector2 Velocity { get { return this.Actor.Velocity; } }
+
+    void OnSpawn()
+    {
+        
+    }
+
+    //public
+}
