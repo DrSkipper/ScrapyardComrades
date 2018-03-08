211c4a8f27f694d623a47c2344398eecba48f1a6
Fletcher <fletcherc89@gmail.com>
2018-03-07 18:01:18 -0800

    working on options for configuring moving platforms

--- Assets/ScrapyardComrades/Scripts/Editing/ObjectConfigurers/PatrollingPlatformConfigurer.cs
+++ Assets/ScrapyardComrades/Scripts/Editing/ObjectConfigurers/PatrollingPlatformConfigurer.cs
@@ -1,9 +1,15 @@
 ﻿using UnityEngine;
+using System.Linq;
 
 public class PatrollingPlatformConfigurer : ObjectConfigurer
 {
     private const string NAME = "PatrollingPlatform";
+    private const string CONFIGURATION = "config";
+    public SpriteRenderer Renderer;
+    public IntegerRectCollider Collider;
     public Transform DestinationLocation;
+    public Sprite[] Configurations;
+    public IntegerVector[] ColliderSizes;
 
     public override Transform GetSecondaryTransform()
     {
@@ -14,12 +20,47 @@ public class PatrollingPlatformConfigurer : ObjectConfigurer
     {
         get
         {
-            return new ObjectParamType[] { };
+            return new ObjectParamType[] {
+                new ObjectParamType(CONFIGURATION, this.Configurations.Select(x => x.name).ToArray())
+            };
         }
     }
 
     protected override void ConfigureParameter(string parameterName, string option)
     {
-        LogInvalidParameter(NAME, parameterName, option);
+        switch (parameterName)
+        {
+            default:
+                LogInvalidParameter(NAME, parameterName, option);
+                break;
+            case CONFIGURATION:
+                configure(option);
+                break;
+        }
+    }
+
+    /**
+     * Private
+     */
+    private void configure(string option)
+    {
+        for (int i = 0; i < this.Configurations.Length; ++i)
+        {
+            if (this.Configurations[i].name == option)
+            {
+                useConfiguration(i);
+                return;
+            }
+        }
+
+        if (this.Configurations.Length > 0)
+            useConfiguration(0);
+        LogInvalidParameter(NAME, CONFIGURATION, option);
+    }
+
+    private void useConfiguration(int config)
+    {
+        this.Renderer.sprite = this.Configurations[config];
+        this.Collider.Size = this.ColliderSizes[config];
     }
 }
