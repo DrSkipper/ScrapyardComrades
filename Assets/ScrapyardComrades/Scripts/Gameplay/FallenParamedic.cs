be99110229c8a2dec1aaf96d597a3a889612337c
Fletcher <fletcherc89@gmail.com>
2018-06-04 16:57:50 -0700

    Added intro paramedic prop and some prop animations

---
+++ Assets/ScrapyardComrades/Scripts/Gameplay/FallenParamedic.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class FallenParamedic : VoBehavior, IPausable
+{
+    public Sprite PreInteractSprite;
+    public Sprite PostInteractSprite;
+
+    void OnSpawn()
+    {
+        _added = false;
+        if (SaveData.DataLoaded)
+        {
+            if (SaveData.PlayerStats.Level > 0)
+            {
+                this.spriteRenderer.sprite = this.PostInteractSprite;
+            }
+            else
+            {
+                this.spriteRenderer.sprite = this.PreInteractSprite;
+                _added = true;
+                GlobalEvents.Notifier.Listen(PlayerHealthController.MUTATE_EVENT, this, onMutate);
+            }
+        }
+    }
+
+    void OnReturnToPool()
+    {
+        if (_added)
+            GlobalEvents.Notifier.RemoveListenersForOwnerAndEventName(this, PlayerHealthController.MUTATE_EVENT);
+    }
+
+    /**
+     * Private
+     */
+    private bool _added;
+
+    private void onMutate(LocalEventNotifier.Event e)
+    {
+        this.spriteRenderer.sprite = this.PreInteractSprite;
+    }
+}
