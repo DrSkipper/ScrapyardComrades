43fcc2fada3ebfc46ee2bb2f6817a696b0811035
Fletcher <fletcherc89@gmail.com>
2018-06-14 15:27:27 -0700

    All three stasis glass break sounds linked to correct frames

--- Assets/ScrapyardComrades/Scripts/Gameplay/MutantVat.cs
+++ Assets/ScrapyardComrades/Scripts/Gameplay/MutantVat.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class MutantVat : VoBehavior
+public class MutantVat : VoBehavior, IPausable
 {
     public PooledObject SpawnPrefab;
     public Transform SpawnPosition;
@@ -9,6 +9,8 @@ public class MutantVat : VoBehavior
     public SCSpriteAnimation BreakAnimation;
     public SCSpriteAnimation BrokenAnimation;
     public SoundData.Key BreakSfxKey;
+    public SoundData.Key CrackSfxKey;
+    public int CrackFrame = 0;
     public string StateKeyForBreak;
 
     public string SpawnEntityKey { get { return _entity.EntityName + StringExtensions.UNDERSCORE + this.SpawnPrefab.name; } }
@@ -52,6 +54,8 @@ public class MutantVat : VoBehavior
             case State.Breaking:
                 if (this.Animator.Elapsed >= this.Animator.CurrentAnimation.LengthInFrames)
                     spawn();
+                else if (this.Animator.Elapsed == this.CrackFrame * this.Animator.GetFrameDuration())
+                    SoundManager.Play(this.CrackSfxKey, this.transform);
                 break;
             case State.Broken:
                 break;
