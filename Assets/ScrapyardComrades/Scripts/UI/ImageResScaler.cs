a4968c780f5618a48dc26384270e6bc91157fa99
Fletcher <fletcherc89@gmail.com>
2018-06-19 16:36:15 -0700

    Intro logo scales if can't fit on screen because of aspect ratio

---
+++ Assets/ScrapyardComrades/Scripts/UI/ImageResScaler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageResScaler : MonoBehaviour
+{
+    public RectTransform ImageTransform;
+    public float AspectCutoff = 1.5f;
+    public float Divisor = 2.0f;
+    bool OffsetYPos = true;
+
+    private void Start()
+    {
+        int w = PlayerPrefs.GetInt(OptionsValues.RESOLUTION_WIDTH_KEY, Screen.currentResolution.width);
+        int h = PlayerPrefs.GetInt(OptionsValues.RESOLUTION_HEIGHT_KEY, Screen.currentResolution.height);
+        float ratio = w / (float)h;
+
+        if (ratio < this.AspectCutoff)
+        {
+            float x = this.ImageTransform.sizeDelta.x;
+            float y = this.ImageTransform.sizeDelta.y;
+            x = Mathf.Round(x / this.Divisor);
+            y = Mathf.Round(y / this.Divisor);
+            this.ImageTransform.sizeDelta = new Vector2(x, y);
+
+            if (this.OffsetYPos)
+            {
+                this.ImageTransform.anchoredPosition = new Vector2(this.ImageTransform.anchoredPosition.x, this.ImageTransform.anchoredPosition.y - Mathf.RoundToInt(y - (y / this.Divisor)));
+            }
+        }
+    }
+}
